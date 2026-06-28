3dd5bdcc1a4ffaceb2e249b25f61aa14f04d8a5b
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2026-06-28 17:51:59 +0700

    fix: delete fallback in next config

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
